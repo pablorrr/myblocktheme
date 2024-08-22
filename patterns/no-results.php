72e3a673af4c75038803d5ed361df01adc37272a
Paweł Kali <pkalisz451@gmail.com>
2024-08-22 12:45:01 +0200

    init repo

---
+++ patterns/no-results.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: No Results Content
+ * Slug: myblocktheme/no-results
+ * Categories: myblocktheme-wp-pattern-cat
+ */
+?>
+<!-- wp:paragraph -->
+<p>
+<?php echo __( 'Sorry, but nothing matched your search terms.', 'myblocktheme' ); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'myblocktheme' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'myblocktheme' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'myblocktheme' ); ?>","buttonUseIcon":true} /-->
